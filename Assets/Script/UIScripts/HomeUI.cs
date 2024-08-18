66903955b366e915afa306fb2f8a78f8163c1233
HIBIKI_5201 <takuto5201@gmail.com>
2024-08-18 14:59:09 +0900

    [update]MomeUIのボタンクリックイベントを最適化

--- Assets/Script/UIScripts/HomeUI.cs
+++ Assets/Script/UIScripts/HomeUI.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,26 +7,35 @@ public class HomeUI : MonoBehaviour
     UIDocument _homeUI;
     VisualElement _root;
 
+    Dictionary<Button, VisualElement> _buttonToWindow;
+
     Button _movementButton;
     Button _collectButton;
     Button _campButton;
     Button _itemButton;
+
+    VisualElement _currentWindow;
 
     VisualElement _movementWindow;
     SliderInt _movementSlider;
 
     private void Start()
     {
+        //UIDocumentの取得
         _homeUI = GetComponent<UIDocument>();
         _root = _homeUI.rootVisualElement;
-
+        //メインボタン系の取得
         _movementButton = _root.Q<Button>("MovementButton");
         _movementButton.clicked += OnClickMovementButton;
         _collectButton = _root.Q<Button>("CollectButton");
+        _collectButton.clicked += OnClickCollectButton;
         _campButton = _root.Q<Button>("CampButton");
+        _campButton.clicked += OnClickCampButton;
         _itemButton = _root.Q<Button>("ItemButton");
-
+        _itemButton.clicked += OnClickItemButton;
+        //Movement関係の取得と初期設定
         _movementWindow = _root.Q<VisualElement>("MovementWindow");
+        _movementWindow.style.display = DisplayStyle.None;
         _movementSlider = _root.Q<SliderInt>("TextBoxSpeedSlider");
         /*
         if (_movementSlider != null)
@@ -39,10 +47,45 @@ public class HomeUI : MonoBehaviour
             });
         }
         */
+
+        //ボタンに対応したウィンドウのエレメントを設定する
+        _buttonToWindow = new()
+        {
+            {_movementButton, _movementWindow},
+            {_campButton, _campButton},
+            {_collectButton, _collectButton},
+            {_itemButton, _itemButton},
+        };
+    }
+    void WindowHide()
+    {
+        _currentWindow.style.display = DisplayStyle.None;
+    }
+
+    void OnclickButton(Button button)
+    {
+        if (_currentWindow != null) WindowHide();
+        _buttonToWindow[button].style.display = DisplayStyle.Flex;
+        _currentWindow = _buttonToWindow[button];
     }
 
     void OnClickMovementButton()
     {
-        _movementWindow.style.display = DisplayStyle.Flex;
+        OnclickButton(_movementButton);
+    }
+
+    void OnClickCollectButton()
+    {
+
+    }
+
+    void OnClickCampButton()
+    {
+
+    }
+
+    void OnClickItemButton()
+    {
+
     }
 }
