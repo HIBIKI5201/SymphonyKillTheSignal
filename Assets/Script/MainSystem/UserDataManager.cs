810d340bada216f16ff5ca9670359a45ab1b261a
HIBIKI_5201 <takuto5201@gmail.com>
2024-08-30 03:27:23 +0900

    [update]データマネージャーに空腹と渇きのメソッドを追加

--- Assets/Script/MainSystem/UserDataManager.cs
+++ Assets/Script/MainSystem/UserDataManager.cs
@@ -19,4 +19,17 @@ public class UserDataManager : MonoBehaviour
         saveData.health += value;
         saveData.health = Mathf.Clamp(saveData.health, 0, 100);
     }
+
+    public void ChangeHunger(int value)
+    {
+        saveData.hunger += value;
+        saveData.hunger = Mathf.Clamp(saveData.hunger, 0, 100);
+    }
+
+    public void ChangeThirst(int value)
+    {
+        saveData.thirst += value;
+        saveData.thirst = Mathf.Clamp(saveData.thirst, 0, 100);
+    }
+
 }
