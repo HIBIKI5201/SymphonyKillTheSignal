8a6863222fcf42d88571bdce05de51e6040244e7
HIBIKI_5201 <takuto5201@gmail.com>
2024-08-30 03:42:01 +0900

    [update]ホームUIのパラメーターを更新するメソッドを追加

--- Assets/Script/UIScripts/HomeUI.cs
+++ Assets/Script/UIScripts/HomeUI.cs
@@ -23,7 +23,9 @@ public class HomeUI : UIBase
     VisualElement _healthBar;
     Label _healthText;
     VisualElement _hungerBar;
+    Label _hungerText;
     VisualElement _thirstBar;
+    Label _thirstText;
 
     VisualElement _currentWindow;
 
@@ -72,7 +74,13 @@ public class HomeUI : UIBase
         _healthText = _root.Q<Label>("Health-Text");
         _healthText.text = $"{SaveDataManager._mainSaveData.health}/100";
         _hungerBar = _root.Q<VisualElement>("Hunger-Bar");
+        _hungerBar.style.width = new Length(SaveDataManager._mainSaveData.hunger, LengthUnit.Percent);
+        _hungerText = _root.Q<Label>("");
+        _hungerText.text = $"{SaveDataManager._mainSaveData.hunger}/100";
         _thirstBar = _root.Q<VisualElement>("Thirst-Bar");
+        _thirstBar.style.width = new Length(SaveDataManager._mainSaveData.thirst, LengthUnit.Percent);
+        _thirstText = _root.Q<Label>("");
+        _thirstText.text = $"{SaveDataManager._mainSaveData.thirst}/100";
         //MovementŠÖŒW‚Ìæ“¾‚Æ‰Šú‰»
         _movementWindow = _root.Q<VisualElement>("MovementWindow");
         _movementWindow.style.display = DisplayStyle.None;
