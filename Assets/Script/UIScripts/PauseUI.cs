40df3de835f054b69ea058d603ab122a2713f385
HIBIKI_5201 <takuto5201@gmail.com>
2024-09-21 13:02:43 +0900

    [add]オーディオの調整ができるスライダーを追加

--- Assets/Script/UIScripts/PauseUI.cs
+++ Assets/Script/UIScripts/PauseUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UIElements;
 
 public class PauseUI : MonoBehaviour
@@ -15,6 +16,21 @@ public class PauseUI : MonoBehaviour
     Button _saveButton;
     Button _optionButton;
     Button _titleButton;
+
+    Slider _audioMasterSlider;
+    Slider _audioBGMSlider;
+    Slider _audioSESlider;
+    Slider _audioVoiceSlider;
+    [SerializeField]
+    AudioMixer _audioMixer;
+    const string _masterName = "Master";
+    float _masterVolume;
+    const string _BGMName = "BGM";
+    float _BGMVolume;
+    const string _SEName = "SoundEffect";
+    float _SEVolume;
+    const string _VoiceName = "Voice";
+    float _VoiceVolume;
 
     void Start()
     {
@@ -37,6 +53,30 @@ public class PauseUI : MonoBehaviour
         _optionButton.clicked += OptionButtonClicked;
         _titleButton = _pauseWindow.Q<Button>("Title");
         _titleButton.clicked += TitleButtonClicked;
+        //オーディオの初期値
+        _audioMixer.GetFloat(_masterName, out _masterVolume);
+        _audioMixer.GetFloat(_BGMName, out _BGMVolume);
+        _audioMixer.GetFloat(_SEName, out _SEVolume);
+        _audioMixer.GetFloat(_VoiceName, out _VoiceVolume);
+        _audioMasterSlider = _root.Q<Slider>("MasterSlider");
+        _audioMasterSlider.RegisterValueChangedCallback(evt => VolumeSliderChanged(_masterName, _masterVolume, _audioMasterSlider.value));
+        VolumeSliderChanged(_masterName, _masterVolume, _audioMasterSlider.value);
+        _audioBGMSlider = _root.Q<Slider>("BGMSlider");
+        _audioBGMSlider.RegisterValueChangedCallback(evt => VolumeSliderChanged(_BGMName, _BGMVolume, _audioBGMSlider.value));
+        VolumeSliderChanged(_BGMName, _BGMVolume, _audioBGMSlider.value);
+        _audioSESlider = _root.Q<Slider>("SESlider");
+        _audioSESlider.RegisterValueChangedCallback(evt => VolumeSliderChanged(_SEName, _SEVolume, _audioSESlider.value));
+        VolumeSliderChanged(_SEName, _SEVolume, _audioSESlider.value);
+        _audioVoiceSlider = _root.Q<Slider>("VoiceSlider");
+        _audioVoiceSlider.RegisterValueChangedCallback(evt => VolumeSliderChanged(_VoiceName, _VoiceVolume, _audioVoiceSlider.value));
+        VolumeSliderChanged(_VoiceName, _VoiceVolume, _audioVoiceSlider.value);
+    }
+
+    void VolumeSliderChanged(string name, float originalVolume, float value)
+    {
+        float db = value / 10 * (originalVolume + 80) - 80;
+        Debug.Log($"{name}の音量は{db}デシベル");
+        _audioMixer.SetFloat(name, db);
     }
 
     void PauseManuReveal()
